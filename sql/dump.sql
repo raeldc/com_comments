fad3009bb3ea7c0da77ee6fe9ca1de7c37730011
Flavio Copes <info@joocode.com>
2010-08-26 19:27:05 +0200

    Added db dump and README file

---
+++ sql/dump.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `jos_comments_comments` (
+  `comments_comment_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `target_type` char(10) NOT NULL,
+  `target_id` int(11) unsigned NOT NULL,
+  `text` text NOT NULL,
+  `created_by` int(11) unsigned NOT NULL,
+  `created_on` datetime NOT NULL,
+  PRIMARY KEY (`comments_comment_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
